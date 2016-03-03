b1a2937e7c104e2034aa8601d4bd614873e8ecbd
private02 <3076954711@qq.com>
2016-03-03 16:36:02 +0800

    Author: 陈磊 Date: 2016-03-03 16:34:54 Task: 继续做 MyAlgorithm.h

--- MyAlgorithm/MyAlgorithm/MyAlgorithm.h
+++ MyAlgorithm/MyAlgorithm/MyAlgorithm.h
@@ -3,12 +3,20 @@
 
 //My_Algorithm:包含一些高效的算法与数据结构（非STL）
 
-
+/*警告：本头文件用C++14编写，请务必使用支持C++14的编译器进行编译。*/
 
 #pragma once
 
 #ifndef _ALGI_
 #define _ALGI_
+
+
+
+
+#define algname_begin  namespace calg{
+
+#define algname_end };
+
 
 #include <algorithm>
 #include <list>
@@ -16,29 +24,63 @@
 #include <process.h>
 #include <set>
 #include <map>
+#include <iostream>
+#include <array>
+
+
+algname_begin
+
+
+//using namespace std;   //防止命名空间污染
+//extern int size;
+/*声明部分*/
 
 template<class T>
 class Qarr {
 public:
 
+	
+	Qarr(int sizes);
 
-	Qarr(int size, int negative):negative(negative) {  }
+	auto operator[](int x);//快速转到定义：  行
 
-
-	T& operator[](int x);
 private:
-	int negative;
-
-
-
+	
+	int size;
+	T* p;
+	
 
 
 
 };
 
+/*定义部分*/
 
 
 
+template<class T>
+inline Qarr<T>::Qarr(int sizes)
+{
+	
+	p = new int[sizes];
+	size = sizes;
+	
+}
+
+template<class T>
+inline auto Qarr<T>::operator[](int x)
+{
+	if (x > size - 1)
+	{
+		std::cout << "Error: An array subscript crossing the line\n";// << std::endl;
+		return p[-1];
+	}
+	return p[x];
+}
+
+
+
+algname_end
 
 
 #endif
---
+++ MyAlgorithm/MyAlgorithm/test.cpp
@@ -0,0 +1,25 @@
+#include "MyAlgorithm.h"
+//#include <array>
+using namespace calg;
+using namespace std;
+int main()
+{
+
+
+	cout << "hello world" << endl;
+	array<int, 10> a = { 0 };
+	array<array<int, 10>, 10> b = { 0 };
+	array<array<array<int,10>, 10>, 10 > c = { 0 };
+	//Qarr<int> qarr(100);
+
+	/*xxx(1);*/
+
+	//qarr[1] = 1;
+	//
+	//cout << qarr[1] << "\n";
+
+	//cout << qarr[1000];
+
+	system("pause");
+	return EXIT_SUCCESS;
+}
